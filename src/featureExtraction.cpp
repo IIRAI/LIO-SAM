628cbeaaf732840b44cf939a186cd6b2fd6a6b0c
ed <edoardo_sorelli@yanmar.com>
2025-07-22 10:07:39 +0200

    improve: separate main from class implementation

--- src/featureExtraction.cpp
+++ src/featureExtraction.cpp
@@ -209,23 +209,3 @@ void FeatureExtraction::publishFeatureCloud()
     // publish to mapOptimization
     pubLaserCloudInfo->publish(cloudInfo);
 }
-
-
-int main(int argc, char** argv)
-{
-    rclcpp::init(argc, argv);
-    rclcpp::NodeOptions options;
-    options.use_intra_process_comms(true);
-    rclcpp::executors::SingleThreadedExecutor exec;
-
-    auto FE = std::make_shared<FeatureExtraction>(options);
-
-    exec.add_node(FE);
-
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[1;32m----> Feature Extraction Started.\033[0m");
-
-    exec.spin();
-
-    rclcpp::shutdown();
-    return 0;
-}
--- src/imageProjection.cpp
+++ src/imageProjection.cpp
@@ -487,22 +487,3 @@ void ImageProjection::publishClouds()
     cloudInfo.cloud_deskewed  = publishCloud(pubExtractedCloud, extractedCloud, cloudHeader.stamp, lidarFrame);
     pubLaserCloudInfo->publish(cloudInfo);
 }
-
-int main(int argc, char** argv)
-{
-    rclcpp::init(argc, argv);
-
-    rclcpp::NodeOptions options;
-    options.use_intra_process_comms(true);
-    rclcpp::executors::MultiThreadedExecutor exec;
-
-    auto IP = std::make_shared<ImageProjection>(options);
-    exec.add_node(IP);
-
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[1;32m----> Image Projection Started.\033[0m");
-
-    exec.spin();
-
-    rclcpp::shutdown();
-    return 0;
-}
